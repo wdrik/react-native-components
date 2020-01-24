db61dcf9fd9bdfe974d89d0c1af432ad11cd290e
iosilva <iosilva@I4PRO08>
2020-01-24 13:18:44 -0300

    DatepickerIOS adjustments

--- src/pages/DatePickerIOSComponent.js
+++ src/pages/DatePickerIOSComponent.js
@@ -1,15 +1,34 @@
 import React, { useState } from "react";
-import { DatePickerIOS, View, StyleSheet } from "react-native";
+import { DatePickerIOS, View, StyleSheet, Button, Text } from "react-native";
 
 export default function DatePickerIOSComponent() {
   const [chosenDate, setChosenDate] = useState(new Date());
+  const [showDatepicker, setShowDatepicker] = useState(false);
 
   return (
     <View style={styles.container}>
-      <DatePickerIOS
-        date={chosenDate}
-        onDateChange={newDate => setChosenDate(newDate)}
-      />
+      {showDatepicker && (
+        <DatePickerIOS
+          date={chosenDate}
+          onDateChange={newDate => setChosenDate(newDate)}
+        />
+      )}
+
+      {showDatepicker && (
+        <Button
+          onPress={() => setShowDatepicker(false)}
+          title="Hide date picker!"
+        />
+      )}
+
+      {!showDatepicker && (
+        <Button
+          onPress={() => setShowDatepicker(true)}
+          title="Show date picker!"
+        />
+      )}
+
+      <Text style={styles.dataText}>{chosenDate.toString()}</Text>
     </View>
   );
 }
@@ -18,5 +37,9 @@ const styles = StyleSheet.create({
   container: {
     flex: 1,
     justifyContent: "center"
+  },
+
+  dataText: {
+    textAlign: "center"
   }
 });
