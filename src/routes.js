987a96d624b6cd011285c4abfd314896ee9b591b
iosilva <iosilva@I4PRO08>
2020-01-30 18:14:59 -0300

    Teste com plugin de ícones

--- src/routes.js
+++ src/routes.js
@@ -1,9 +1,9 @@
 import React from "react";
 import { createAppContainer } from "react-navigation";
 import { createDrawerNavigator } from "react-navigation-drawer";
-import Icon from "react-native-vector-icons/FontAwesome";
+import Icon from "react-native-vector-icons/FontAwesome5";
 
-Icon.loadFont();
+// Icon.loadFont();
 
 import Home from "./pages/Home";
 import FadeAnimation from "./pages/FadeAnimation";
@@ -28,126 +28,133 @@ const mainNavigation = createDrawerNavigator({
   Home: {
     screen: Home,
     navigationOptions: () => ({
-      drawerIcon: <Icon name="lock" size={20} color="#444" />
+      drawerIcon: <Icon name="apple" size={20} color="#444" />
     })
   },
 
   FadeAnimation: {
     screen: FadeAnimation,
     navigationOptions: () => ({
-      drawerIcon: <Icon name="rocket" size={20} color="#444" />
+      drawerIcon: <Icon name="apple-alt" size={20} color="#444" />,
+      headerStyle: {
+        backgroundColor: "#f4511e"
+      },
+      headerTintColor: "#fff",
+      headerTitleStyle: {
+        fontWeight: "bold"
+      }
     })
   },
 
   ClickAnimation: {
     screen: ClickAnimation,
     navigationOptions: () => ({
-      drawerIcon: <Icon name="rocket" size={20} color="#444" />
+      drawerIcon: <Icon name="apple" size={20} color="#444" />
     })
   },
 
   ImageComponent: {
     screen: ImageComponent,
     navigationOptions: () => ({
-      drawerIcon: <Icon name="rocket" size={20} color="#444" />
+      drawerIcon: <Icon name="apple" size={20} color="#444" />
     })
   },
 
   ActivityindicatorComponent: {
     screen: ActivityindicatorComponent,
     navigationOptions: () => ({
-      drawerIcon: <Icon name="rocket" size={20} color="#444" />
+      drawerIcon: <Icon name="apple" size={20} color="#444" />
     })
   },
 
   ButtonComponent: {
     screen: ButtonComponent,
     navigationOptions: () => ({
-      drawerIcon: <Icon name="rocket" size={20} color="#444" />
+      drawerIcon: <Icon name="apple" size={20} color="#444" />
     })
   },
 
   DatePickerIOSComponent: {
     screen: DatePickerIOSComponent,
     navigationOptions: () => ({
-      drawerIcon: <Icon name="rocket" size={20} color="#444" />
+      drawerIcon: <Icon name="apple" size={20} color="#444" />
     })
   },
 
   InputAccessoryViewComponent: {
     screen: InputAccessoryViewComponent,
     navigationOptions: () => ({
-      drawerIcon: <Icon name="rocket" size={20} color="#444" />
+      drawerIcon: <Icon name="apple" size={20} color="#444" />
     })
   },
 
   MaskedViewIOS: {
     screen: MaskedViewIOS,
     navigationOptions: () => ({
-      drawerIcon: <Icon name="rocket" size={20} color="#444" />
+      drawerIcon: <Icon name="apple" size={20} color="#444" />
     })
   },
 
   ModalComponent: {
     screen: ModalComponent,
     navigationOptions: () => ({
-      drawerIcon: <Icon name="rocket" size={20} color="#444" />
+      drawerIcon: <Icon name="apple" size={20} color="#444" />
     })
   },
 
   PickerComponent: {
     screen: PickerComponent,
     navigationOptions: () => ({
-      drawerIcon: <Icon name="rocket" size={20} color="#444" />
+      drawerIcon: <Icon name="apple" size={20} color="#444" />
     })
   },
 
   RefreshControlComponent: {
     screen: RefreshControlComponent,
     navigationOptions: () => ({
-      drawerIcon: <Icon name="rocket" size={20} color="#444" />
+      drawerIcon: <Icon name="apple" size={20} color="#444" />
     })
   },
 
   SafeAreaViewComponent: {
     screen: SafeAreaViewComponent,
     navigationOptions: () => ({
-      drawerIcon: <Icon name="rocket" size={20} color="#444" />
+      drawerIcon: <Icon name="apple" size={20} color="#444" />
     })
   },
 
   ScrollViewComponent: {
     screen: ScrollViewComponent,
     navigationOptions: () => ({
-      drawerIcon: <Icon name="rocket" size={20} color="#444" />
+      drawerIcon: <Icon name="apple" size={20} color="#444" />
     })
   },
 
   SectionListComponent: {
     screen: SectionListComponent,
     navigationOptions: () => ({
-      drawerIcon: <Icon name="rocket" size={20} color="#444" />
+      drawerIcon: <Icon name="apple" size={20} color="#444" />
     })
   },
 
   SegmentedControlIOSComponent: {
     screen: SegmentedControlIOSComponent,
     navigationOptions: () => ({
-      drawerIcon: <Icon name="rocket" size={20} color="#444" />
+      drawerIcon: <Icon name="apple" size={20} color="#444" />
     })
   },
 
   SliderComponent: {
     screen: SliderComponent,
     navigationOptions: () => ({
-      drawerIcon: <Icon name="rocket" size={20} color="#444" />
+      drawerIcon: <Icon name="apple" size={20} color="#444" />
     })
   },
 
   TextInputComponent: {
     screen: TextInputComponent,
     navigationOptions: () => ({
-      drawerIcon: <Icon name="rocket" size={20} color="#444" />
+      drawerIcon: <Icon name="apple" size={20} color="#444" />
     })
   }
 });
